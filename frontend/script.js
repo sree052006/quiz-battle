9d00ecedd76c5c815f0d98d0db647de5f06a49ec
Sreelakshmi <sreelakshmikarukachal@gmail.com>
2026-06-12 13:31:41 +0530

    created home page

---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+document.getElementById("loginBtn").addEventListener("click", function () {
+    alert("Login page coming soon!");
+});
+
+document.getElementById("registerBtn").addEventListener("click", function () {
+    alert("Register page coming soon!");
+});
